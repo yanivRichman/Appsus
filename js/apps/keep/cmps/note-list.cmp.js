ca8beadefbaa29c4c0db0dd41395431ba782d5b0
Revital Elishkov <revitalrle@gmail.com>
2021-11-11 16:43:43 +0200

    colors start

--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -10,19 +10,21 @@ export default {
     template: `
     <section class="note-list flex space-between">
     <!-- <div class="note-list">  -->
-        <div v-for="note in notes" :key="note.id" class="notes-preview-container">
+        <div v-for="note in notes" :key="note.id">
+            <div class="notes-preview-container" style="backgroundColor:red;">
             <note-preview :note="note"/>  
             <div class="note-footer"> 
                 <button @click="remove(note.id)">X</button>
                 <!-- <button @click="openColors">🎨</button> -->
+                <div class="colorsPalete">
+                <!-- <button @click="openColors"></button> -->
+                </div>
                 <!-- <div class="colors"> -->
                 <!-- <button @click="pinNote">📍</button>
                 <button @click="duplicateNote">duplicate</button> -->
-
-
-
-                
             </div>
+        </div>
+
                      
         </div>
     <!-- </div> -->
@@ -34,7 +36,7 @@ export default {
             this.$emit('remove', noteId)
         }
     },
-    openColors(){
+    openColors() {
 
     }
 }
