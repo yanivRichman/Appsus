95e0e5c9b808ab5625d937b0dee23aeaa0b28e4f
Yaniv Richman <yaniv4980@gmail.com>
2021-11-14 01:33:04 +0200

    home page

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,15 +2,15 @@
 export default {
     template: `
    <section> 
-       <h3>Let's help you Get things DONE!</h3>
+       <h1>Let's help you Get things DONE!</h1>
        <div class="flex space-around">
        <div class="">
-             <p> Meet your new Email inbox. decide which emails you want to read and when. </p>
+             <p class="home-txt"> Meet your new Email inbox. <br> decide which emails you want to read and when. </p>
         
            <img class="email-img" src="../../img/email.png"/>
        </div>
        <div>
-            <p> Capture what’s on your mind. Add notes, lists, photos, and video to Keep.</p>
+            <p class="home-txt"> Capture what’s on your mind.<br> Add notes, lists, photos, and video to Keep.</p>
            <img src="../../img/notes.png"/>
         </div>
        </div>
