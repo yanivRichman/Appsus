f981bf503b121318865e0d1bda378727f1eda10f
Revital Elishkov <revitalrle@gmail.com>
2021-11-14 00:58:46 +0200

    home page photos

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,7 +2,18 @@
 export default {
     template: `
    <section> 
-       <h3>Home</h3>
+       <h3>Let's help you Get things DONE!</h3>
+       <div class="flex space-around">
+       <div class="">
+             <p> Meet your new Email inbox. decide which emails you want to read and when. </p>
+        
+           <img class="email-img" src="../../img/email.png"/>
+       </div>
+       <div>
+            <p> Capture what’s on your mind. Add notes, lists, photos, and video to Keep.</p>
+           <img src="../../img/notes.png"/>
+        </div>
+       </div>
    </section>
 `,
 }
